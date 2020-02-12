2e070c369ee9bf8eb6fb404dbdaf6598b4053005
krak <kraksin12@gmail.com>
2020-02-12 15:24:21 +0900

    tef

---
+++ upload_test_app.sh
@@ -0,0 +1,19 @@
+# git config --name
+git config --global user.name "kraksin"
+
+# git config --email
+git config --global user.name "kraksin12@gmail.com"
+
+# clone git SSH
+git clone git@github.com:operate2v/operate2v.github.io.git
+
+cd opreate2v.github.io && touch test.txt
+
+# git add *
+git add *
+
+# git commit
+git commit -m "contentsfly test add [CircleCI]"
+
+# git push
+git push origin master
